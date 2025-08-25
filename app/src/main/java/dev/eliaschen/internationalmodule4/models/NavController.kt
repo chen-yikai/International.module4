0632271e32a655f245366b5d0be418fd8d3a44c4
elias <eliaschen.dev@gmail.com>
2025-08-25 21:01:13 +0800

    update entry

--- app/src/main/java/dev/eliaschen/internationalmodule4/models/NavController.kt
+++ app/src/main/java/dev/eliaschen/internationalmodule4/models/NavController.kt
@@ -6,7 +6,7 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class NavController : ViewModel() {
-    private val initNav = Screen.Exhibit
+    private val initNav = Screen.Home
     var currentNav by mutableStateOf(initNav)
     val navStack = mutableListOf(initNav)
 
